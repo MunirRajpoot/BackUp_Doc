973ab6b23e89944acb6f60133f9ecff318cb1d4f
MunirRajpoot <munirrajpoot1012@gmail.com>
2025-03-14 23:13:00 +0500

    fix error

--- processor/src/app/layout.js
+++ processor/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
+import StyledComponentsRegistry from "./StyledComponentsRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar/>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
   );
