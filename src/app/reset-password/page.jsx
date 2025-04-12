aff29e09c9eaeffc15d75706df7b416c6b5fbbb4
MunirRajpoot <munirrajpoot1012@gmail.com>
2025-04-12 21:33:11 +0500

    Add #forgot Password #Reset Password

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -8,16 +8,25 @@ const Page = () => {
         <div className="flex justify-center align-items-center items-start h-screen overflow-y-auto pt-8">
             <div className="flex flex-col items-center bg-white text-black w-full max-w-md p-8 rounded-xl shadow-md">
                 <div className="text-center mb-3">
-                    <h2>Forgot Password</h2>
+                    <h2>Reset Password</h2>
                 </div>
 
                 <form className="w-full space-y-4">
                     <div>
-                        <label htmlFor="email" className="block mb-1 font-medium">Email address</label>
+                        <label htmlFor="nPassword" className="block mb-1 font-medium">New Password</label>
                         <input
-                            type="email"
-                            id="email"
-                            placeholder="Enter your email address"
+                            type="password"
+                            id="nPassword"
+                            placeholder="Create a new password"
+                            className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block mb-1 font-medium">Confirm Password</label>
+                        <input
+                            type="cPassword"
+                            id="cPassword"
+                            placeholder="Confirm your password"
                             className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
@@ -25,10 +34,6 @@ const Page = () => {
                         Submit
                     </button>
 
-                    <p className="text-sm text-center">
-                        <span className=''>Remember you Password?{' '}</span>
-                        <Link href="/login" className="text-blue-600 hover:underline ">login</Link>
-                    </p>
                 </form>
             </div>
         </div>
